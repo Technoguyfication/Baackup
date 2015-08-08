8866ea4ee62ef55d193118b0d8f13718d4348190
Hayden Andreyka <haydenandreyka@yahoo.com>
2015-08-08 12:51:51 -0700

    Fixing gitignore part 2

---
+++ Baackup/Baackup/ConsoleTools.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baackup
+{
+    public class ConsoleTools
+    {
+        public static void Print(string message, bool line = true)
+        {
+            if (line)
+                Console.WriteLine(message);
+            else
+                Console.Write(message);
+        }
+
+        public static void Pause(string message = "")
+        {
+            if (!(message == ""))
+                Print(message);
+            Console.ReadKey(true);
+        }
+
+        public static void Title(string title)
+        {
+            Console.Title = title;
+        }
+
+        public static void Exit(int code = 0)
+        {
+            Environment.Exit(code);
+        }
+
+        public static void Clear()
+        {
+            Console.Clear();
+        }
+    }
+}
