51a6b25dbaaad46461e45b7672441bd36943c7ac
Hayden Andreyka <haydenandreyka@yahoo.com>
2015-08-09 15:39:43 -0700

    started adding default config variables

--- BaackupConfig/XMLConfig.cs
+++ BaackupConfig/XMLConfig.cs
@@ -15,7 +15,30 @@ namespace BaackupConfig
 
         public static void GenerateDefaultConfig()
         {
+            // RCON
+            Variables.usercon = false;
+            Variables.rconhostname = "127.0.0.1";
+            Variables.rconpass = null;
+            Variables.rconport = 25575;
+            
+            // Worlds container
+            Variables.worldscontaineractive = false;
+            Variables.worldscontainerpath = null;
 
+            // Backup Messages
+            Variables.backupmsgactive = false;
+            Variables.backupmsg = "say The server is backing up! Prepare for some slight lag!";
+
+            Variables.backupfinishmsgactive = false;
+            Variables.backupfinishmsg = "say Server Backup Completed!";
+
+            // Backup container and Tmp
+            Variables.backupcontainer = null;
+            Variables.usecustomtmpdir = false;
+            Variables.customtmpdir = null;
+
+            // Backup ID
+            Variables.backupscustomidprefix = null;
         }
 
         public static bool DoesConfigExist()
