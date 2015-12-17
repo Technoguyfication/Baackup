409c2ef4a01b829a8ada8adc410130d16991a78b
Hayden (Technoguyfication) Andreyka <haydenandreyka@gmail.com>
2015-12-16 18:27:35 -0700

    Added static Main() method to Baackup Config
    Now will travis stop complaining?

---
+++ BaackupConfig/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaackupConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // There, I made an entry point. Happy now?
+        }
+    }
+}
