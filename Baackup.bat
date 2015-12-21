41ba247eca2d9a877692590b83e16e3086941a6a
Hayden (Technoguyfication) Andreyka <haydenandreyka@gmail.com>
2015-12-21 16:09:45 -0700

    Updated batch script
    Now I won't forget to do it later.
    
    And I will do that :stuck_out_tongue_closed_eyes:

--- Baackup.bat
+++ Baackup.bat
@@ -1,15 +1,12 @@
 @echo off
-rem This is the launch script for the backup program!
-rem The server will call this script upon backup.
+rem -- This is the launch script for the backup program!
+rem -- The server will call this script upon backup.
 
-rem Change to current directory and drive. NOTE: This may make it impossible to be run if the server is on a network path, please map the drive.
+rem -- Change to current directory and drive. NOTE: This may make it impossible to be run if the server is on a network path, please map the drive.
 %~d0
 cd %~dp0
 
-rem Start program.
+rem -- Start program.
 
-echo [Baackup] Starting backup software.. please wait..
-"%~dp0\Baackup.exe" "%cd%"
-if ERRORLEVEL==0 ( echo [Baackup] Backup software reported finish. )
-if not ERRORLEVEL==0 ( echo [Baackup] Backup software reported failure. )
+"%~dp0\Baackup\Baackup.exe" "%cd%"
 exit
